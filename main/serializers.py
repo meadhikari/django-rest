3972ff6c2c041c8799fa3600ea088eb889220d01
Bikram Adhikari <salik.adhikari@gmail.com>
2017-03-31 22:48:07 +0545

    asyncwala function

--- main/serializers.py
+++ main/serializers.py
@@ -19,7 +19,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'name')
 
 class ImageSerializer(serializers.ModelSerializer):
-    def postExec(output,error,pk):
+    def postExec(self,output,error,pk):
         obj = ImageProcessing.objects.get(pk=pk)
         obj.output = output
         obj.error = error
@@ -30,25 +30,26 @@ class ImageSerializer(serializers.ModelSerializer):
         validated_data.pop('output')
         validated_data.pop('error')
         img_obj = ImageProcessing.objects.create(output="Processing...", error='', **validated_data)
-        def runInThread(onExit, popenArgs):
-            image = validated_data['image']
-            # md5 = envoy.run("md5 tmp/"+image.name).std_out
-            # md5 = envoy.run("sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ tmp/"+image.name)
-            some_command = "sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ /home/anjani/static/media/tmp/"+image.name.replace(" ","_")
-            #some_command = "md5 tmp/" + image.name
-            print(some_command)
-            p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
-            (output, err) = p.communicate()
-            p_status = p.wait()
-            if err:
-                error = err.replace("\n", "")
-            else:
-                error = ""
-            onExit(output,error,img_obj.pk)
+        def asyncwala(onExit,popenArgs):
+            def runInThread(onExit, popenArgs):
+                image = validated_data['image']
+                # md5 = envoy.run("md5 tmp/"+image.name).std_out
+                # md5 = envoy.run("sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ tmp/"+image.name)
+                some_command = "sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ /home/anjani/static/media/tmp/"+image.name.replace(" ","_")
+                #some_command = "md5 tmp/" + image.name
+                print(some_command)
+                p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
+                (output, err) = p.communicate()
+                p_status = p.wait()
+                if err:
+                    error = err.replace("\n", "")
+                else:
+                    error = ""
+                onExit(output,error,img_obj.pk)
             thread = multiprocessing.Process(target=runInThread, args=(onExit, popenArgs))
             thread.start()
-            return image
-        return runInThread(self.postExec,"")
+        asyncwala(self.postExec,"")
+        return img_obj
 
     class Meta:
         model = ImageProcessing
