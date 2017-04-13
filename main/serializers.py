4651865fc3441074ad8f81ee89d7cae5f7d8cd0a
Ubuntu <ubuntu@ip-172-31-29-118.eu-west-2.compute.internal>
2017-04-13 08:27:36 +0000

    server change

--- main/serializers.py
+++ main/serializers.py
@@ -35,7 +35,7 @@ class ImageSerializer(serializers.ModelSerializer):
                 image = validated_data['image']
                 # md5 = envoy.run("md5 tmp/"+image.name).std_out
                 # md5 = envoy.run("sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ tmp/"+image.name)
-                some_command = "sh /home/anjani/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ /home/anjani/static/media/tmp/"+image.name.replace(" ","_")
+                some_command = "sh /home/ubuntu/VISULYTIX_COMPILED_TOOL/run_VISULYTIX_COMPILED_TOOL.sh /usr/local/MATLAB/MATLAB_Runtime/v91/ /home/ubuntu/static/media/tmp/"+image.name.replace(" ","_")
                 #some_command = "md5 tmp/" + image.name
                 print(some_command)
                 p = subprocess.Popen(some_command, stdout=subprocess.PIPE, shell=True)
