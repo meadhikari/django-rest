d0b0d2231117850b7a14928c73ecde6e9a4f98a4
Bikram Adhikari <salik.adhikari@gmail.com>
2017-05-26 14:53:45 +0545

    binary admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from main.models import Binary
 # Register your models here.
+
+class BinaryAdmin(admin.ModelAdmin):
+    #Maybe get all landing pages last known ecpc?
+    list_display = ('title', 'file','default')
+admin.site.register(Binary, BinaryAdmin)
