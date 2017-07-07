edd053f853cccd6dc7a754cbc0009484bcb8c79c
Bikram Adhikari <salik.adhikari@gmail.com>
2017-07-07 17:24:51 +0545

    user model expansion with usage record

--- main/models.py
+++ main/models.py
@@ -5,6 +5,9 @@ from django.core.files.storage import FileSystemStorage
 from .fields import UniqueBooleanField
 import zipfile
 import time
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+
 from django.db.models.signals import post_save
 unique_folder_name = int(time.time())
 fs = FileSystemStorage(location='/home/ubuntu/VISULYTIX_COMPILED_TOOL')
@@ -15,6 +18,10 @@ class ImageProcessing(models.Model):
     output = models.CharField(max_length=500,blank=True)
     error = models.CharField(max_length=500, blank=True)
     user = models.ForeignKey(to=User, related_name="user", null=True, blank=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    usage = models.IntegerField(default=0,max_length=3)
 
 class Binary(models.Model):
     title = models.CharField(max_length=50)
@@ -31,5 +38,14 @@ def post_saveops(sender, **kwargs):
      zip_ref.close()
 
 post_save.connect(post_saveops, sender=Binary)
- 
 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
