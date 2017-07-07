265be106bcdbb84a74f9e6ec97ef60471b966b11
Bikram Adhikari <salik.adhikari@gmail.com>
2017-07-07 17:27:31 +0545

    Profile in Admin Added

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Binary
+from main.models import Binary,Profile
 # Register your models here.
 
 class BinaryAdmin(admin.ModelAdmin):
@@ -7,3 +7,8 @@ class BinaryAdmin(admin.ModelAdmin):
     list_display = ('title', 'file','default')
     list_editable = ('default',)
 admin.site.register(Binary, BinaryAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    #Maybe get all landing pages last known ecpc?
+    list_display = ('user', 'usage')
+admin.site.register(Profile, ProfileAdmin)
